8bbc4765171289a6b2ca32ef9fed01dc6d988688
Matt Jennings <matt@mattjennings.net>
2019-06-27 20:36:32 -0700

    added data-state, data-still, data-animate attributes

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,69 @@
 $(document).ready(function() {
 
+    function CreateGiphyImages() {
+        var $buttonsContainer = $('#buttons-container');
+        var $gifsContainer = $('#gifs-container');
+
+        var topics = [
+            'basketball',
+            'rugby',
+            'running',
+            'tennis',
+            'soccer'
+        ];
+
+        this.createButtons = function() {
+            topics.map(function(btn) {
+                $buttonsContainer.append('<button data-gif=' + btn + '>' + btn + '</button>');
+            });
+        };
+        
+        this.clickBtns = function(btn) {
+            $(document).on('click', btn, function() {
+                var sport = $(this).attr('data-gif');
+
+                var apiKey = 'LCb1JsqOsoUxBDkNU3BL8xN709losvDO';
+
+                var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' +
+                sport + '&api_key=' + apiKey + '&limit=10&rating=g';
+
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function(response) {
+                    var results = response.data;
+
+                    results.map(function(item) {
+                        console.log(item);
+                        var giphyImg = $('<img>');
+                        giphyImg
+                            .attr({
+                                'data-state': 'still',
+                                'data-still': item.images.fixed_height_still.url,
+                                'data-animate': item.images.fixed_height.url,
+                                src: item.images.fixed_height_still.url
+                                
+                            });
+                        var imageAndParagraph = $('<p>').append(giphyImg);
+    
+                        $gifsContainer.append(imageAndParagraph);
+                    });
+
+                });
+
+            });
+        }
+
+    } 
+
+
+    var createGiphyImagesObj = new CreateGiphyImages();
+
+    createGiphyImagesObj.createButtons();
+
+    createGiphyImagesObj.clickBtns('button');
+
+
+
+
 });
